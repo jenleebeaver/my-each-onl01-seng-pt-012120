193495a2b3ce47dd52b7000246eeefbc305d0498
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-28 23:48:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,12 +4,7 @@ def my_each(array) # put argument(s) here
 
   while i > array.length
     i += 1
-    #puts i
-    #if i == 4
-      #break
   end
 end
 
-#my_each(array) do |array|
-  #puts array
-#end
+
